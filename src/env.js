bd9aa93fe338582d7e1d057187d8bb9a51d97462
desfire <alberto.santos.225@gmail.com>
2021-05-18 23:11:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/env.js
@@ -0,0 +1,11 @@
+export const config = {
+    ENDPOINT: "https://v1.nocodeapi.com/fansz/uptime/TCtYrRDnljUSAwet?monitors=788155192-780132861",
+
+    title: "UpTime",
+
+    website: "https://status.fansz.club",
+
+    twitterHandle: "@fanscityeu",
+    
+    twitterURL: "https://twitter.com/fanscityeu"
+}
